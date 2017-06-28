0acec0db281d354abaa255fed264eccc98742581
mayomayo25 <10bvb25hal45016@gmail.com>
2017-06-28 18:44:53 +0900

    [update] 診断ページにスムーズスクロールを適用 / ナビゲーションをクリック時にアニメーション

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
-//無限にループ
+/* ----------------------------------------
+fileName : main.js
+writer   : Daichi Seki, Miwa Shuntaro
+---------------------------------------- */
+
 $(function(){
-$("body").height($(document).height());　$(window).scroll(function() {
-if
-($(window).scrollTop() >= ($("body").height() - $(window).height())) {　$(window).scrollTop(1);}
-else if
-($(window).scrollTop() == 0) {　$(window).scrollTop($("body").height() - $(window).height() - 1);}});
+
+  // diagnosis.html
+  // headerから各問題へスムーズスクロール
+  $('a[href^="#"]').on('click', function(){
+    // ナビゲーションの色を変える
+    $(".question_list").find('li.question_list__item').removeClass('active')
+    $(this).parent().addClass('active');
+
+    // 速度
+    var speed = 500;
+    var $href= $(this).attr("href");
+    var target = $($href == "#" || $href == "" ? 'html' : $href);
+
+    // 固定ヘッダーの高さ
+    var fixedHeaderHeight = 210;
+    var position = target.offset().top - fixedHeaderHeight;
+
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false
+  });
 });
