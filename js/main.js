4eab0b17431028bd142959f3c58b1bf4630ed2ed
mayomayo25 <10bvb25hal45016@gmail.com>
2017-07-05 16:45:23 +0900

    [update] 診断ページ/ナビゲーションをクリックした際にこのはちゃんの表示を切り替える(仮)

--- js/main.js
+++ js/main.js
@@ -2,26 +2,20 @@
 fileName : main.js
 writer   : Daichi Seki, Miwa Shuntaro
 ---------------------------------------- */
-// //トップページheaderの高さ
-// var $header__height = $('.header_wrapp').outerWidth(true);
-// //ウィンドウの高さ
-// var $window__height=window.innerHeight;
-//
-// alert($header__height);
+
 $(function(){
-
-  // $("body").height($(document).height() -  + $window__height);　$(window).scroll(function() {
-  // if
-  // ($(window).scrollTop() >= ($("body").height() - $(window).height())) {　$(window).scrollTop(1);}
-  // else if
-  // ($(window).scrollTop() == 0) {　$(window).scrollTop($("body").height() - $(window).height() - 1);}});
-
   // diagnosis.html
   // headerから各問題へスムーズスクロール
   $('a[href^="#"]').on('click', function(){
     // ナビゲーションの色を変える
     $(".question_list").find('li.question_list__item').removeClass('active')
+    // 親要素のliに.activeを付与
     $(this).parent().addClass('active');
+
+    // このはちゃんの画像を非表示に
+    $(".question_list").find('li.question_list__item img.image').removeClass('active');
+    // クリックした要素にこのはちゃんを表示
+    $(this).find('img.image').addClass('active');
 
     // 速度
     var speed = 1000;
