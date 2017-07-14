4c03dd2467e017e0b0064fdf5d3247f596a57979
三輪　俊幸 <>
2017-07-15 02:42:31 +0900

    [update]ローディングアニメーション修正

--- js/libs/loading.js
+++ js/libs/loading.js
@@ -14,8 +14,6 @@ $(function(){
 
   $(window).load(function () { //全ての読み込みが完了したら実行
     //フェードアウトしていくようにする
-    // $('#loader-bg').delay(900).fadeOut(1500);
-    // $('#loader').delay(600).fadeOut(1000);
     $('#loader-bg').delay(1200).fadeOut(1800);
     $('#loader').delay(900).fadeOut(1500);
     //コンテンツの表示
@@ -24,7 +22,7 @@ $(function(){
 });
 
 function stopload(){
-$('#wrap').css('display','block');
-$('#loader-bg').delay(900).fadeOut(800);
-$('#loader').delay(600).fadeOut(300);
+  $('#wrap').css('display','block');
+  $('#loader-bg').delay(900).fadeOut(800);
+  $('#loader').delay(600).fadeOut(300);
 }
